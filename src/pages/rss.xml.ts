fc9c422cf55c49e209c3cfadebe212500e5f18c6
chacat68 <cainefox@gmail.com>
2025-02-23 21:08:41 +0800

    在RSS XML中添加follow_challenge元素，包含feedId和userId

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,6 +16,12 @@ export async function GET(context: APIContext) {
       pubDate: post.data.date,
       description: post.data.summary,
     })),
-    customData: `<language>${site.lang}</language>`,
+    customData: `<language>${site.lang}</language>
+    <follow_challenge>
+      <feedId>106394337974619136</feedId>
+      <userId>51734640829292544</userId>
+    </follow_challenge>
+    <!-- 在这里添加您的 XML 代码 -->
+    `,
   })
 }
